d18bef1b6bc9b609113f5d23165bddeab4d7a100
metadream <>
2023-11-07 16:56:17 +0800

    Update Component.js

--- src/components/Component.js
+++ src/components/Component.js
@@ -28,12 +28,12 @@ export class Component extends HTMLElement {
     }
 
     connectedCallback() {
-        const shadow = this.attachShadow({ mode: 'open' });
+        const shadowRoot = this.attachShadow({ mode: 'open' });
 
         // Add styles
         const style = createElement('style');
         style.textContent = shadowStyles + (this.styles || '');
-        shadow.append(style);
+        shadowRoot.append(style);
 
         // Add attributes
         const names = this.getAttributeNames();
@@ -43,12 +43,12 @@ export class Component extends HTMLElement {
         this.template = this.template.replace(/{{[a-zA-Z0-9\-]+}}/g, '');
 
         // Create shadow body
-        const body = createElement(this.template);
-        shadow.body = body;
-        shadow.append(body);
+        const shadowBody = createElement(this.template);
+        this.shadowBody = shadowBody;
+        shadowRoot.append(shadowBody);
 
         // Components callback
-        this.onConnected && this.onConnected(shadow);
+        this.onConnected && this.onConnected();
     }
 
 }
