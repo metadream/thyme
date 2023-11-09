97f22f3b7773581ba7791dca4475c56c04ab7085
metadream <>
2023-11-09 13:57:36 +0800

    slot

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -4,21 +4,18 @@ import { Component } from './Component.js';
 export class Toast extends Component {
 
     styles = toastStyles;
-    template = `
-        <div class="quick-overlay quick-toast">
-            <div class="{{type}}">{{text}}</div>
-        </div>
-    `;
+    template = `<div class="quick-overlay quick-toast"><div class="{{type}}"><slot></slot></div></div>`;
 
     onConnected() {
         // Show instance
-        this.shadowBody.addClass('quick-fade-in');
+        const body = this.shadowBody;
+        body.addClass('quick-fade-in');
 
         // Hide delay
         const delay = this.getAttribute('delay') || 3000;
         setTimeout(() => {
-            this.shadowBody.addClass('quick-fade-out');
-            this.shadowBody.on('animationend', () => this.remove());
+            body.addClass('quick-fade-out');
+            body.on('animationend', () => this.remove());
         }, delay);
     }
 
