72fb2b1d2430e9e14d4cf59325f64fb34e36a8a8
metadream <>
2023-11-09 14:05:54 +0800

    Update Field.js

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,17 +1,45 @@
 import fieldStyles from '../styles/field.css';
 import calendarIcon from "../icons/calendar.svg";
-import { createElement, toDataUri } from '../modules/Util.js';
+import { createElement, toDataURI } from '../modules/Util.js';
 import { Component } from './Component.js';
 
 export class Field extends Component {
 
     styles = fieldStyles;
-    template = `
-        <div class="quick-field">
-            <label>{{label}}</label>
-            <input type="{{type}}" value="{{value}}"/>
-        </div>
-    `;
+    template = `<div class="quick-field"><label>{{label}}</label><input type="{{type}}" value="{{value}}"/></div>`;
+
+    onConnected() {
+        const input = this.getElement('input');
+        this.keyElement = input;
+
+        if (this.getAttribute('type') == 'calendar') {
+            input.readOnly = true;
+            input.addClass('icon-trigger');
+            input.style.backgroundImage = `url(${toDataURI(calendarIcon)})`;
+
+            input.on('click', () => {
+                const $calendar = createElement('quick-calendar');
+                this.shadowRoot.append($calendar);
+
+                $calendar.setAttribute('lang', this.getAttribute('lang') || '');
+                $calendar.attach(input);
+                $calendar.on('selected', (e) => {
+                    input.value = e.detail;
+                    $calendar.remove();
+                });
+            });
+        }
+
+        if (this.hasAttribute('required')) {
+            this.shadowBody.addClass('required');
+        }
+        if (this.hasAttribute('readonly')) {
+            input.readOnly = true;
+        }
+        if (this.hasAttribute('disabled')) {
+            input.disabled = true;
+        }
+    }
 
     get value() {
         return this.keyElement.value;
@@ -19,40 +47,6 @@ export class Field extends Component {
 
     set value(v) {
         this.keyElement.value = v;
-    }
-
-    onConnected() {
-        this.keyElement = this.getElement('input');
-
-        if (this.getAttribute('type') == 'calendar') {
-            this.keyElement.readOnly = true;
-            this.keyElement.addClass('icon-trigger');
-            this.keyElement.style.backgroundImage = `url(${toDataUri(calendarIcon)})`;
-
-            this.keyElement.on('click', () => {
-                const $calendar = createElement('quick-calendar');
-                this.shadowRoot.append($calendar);
-
-                $calendar.setAttribute('lang', this.getAttribute('lang') || '');
-                $calendar.attach(this.keyElement);
-                $calendar.on('selected', (e) => {
-                    this.keyElement.value = e.detail;
-                    $calendar.remove();
-                });
-            });
-        }
-
-        if (this.hasAttribute('disabled')) {
-            this.keyElement.disabled = true;
-        }
-
-        if (this.hasAttribute('readonly')) {
-            this.keyElement.readOnly = true;
-        }
-
-        if (this.hasAttribute('required')) {
-            this.shadowBody.classList.add('required');
-        }
     }
 
 }
