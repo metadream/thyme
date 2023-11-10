82ee643fc462b2e2e5b6c642ec2f728d3df9e335
metadream <>
2023-11-10 10:24:25 +0800

    ripple

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,22 +4,14 @@ import { Component } from './Component.js';
 export class Button extends Component {
 
     styles = styles;
-    template = `<button class="{{class}}"><slot></slot><div class="ripple"></div></button>`;
+    template = `<button class="{{class}}"><slot></slot></button>`;
 
     onConnected() {
         const button = this.shadowBody;
-        const ripple = this.getElement('.ripple');
 
         if (this.hasAttribute('disabled')) {
             button.setAttribute('disabled', true);
         }
-
-        button.on('mousedown', () => {
-            const rect = button.getBoundingClientRect();
-            const size = (rect.width > rect.height ? rect.width : rect.height) * 2 + 'px';
-            ripple.style.width = size;
-            ripple.style.height = size;
-        });
     }
 
 }
