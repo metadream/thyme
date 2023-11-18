fea2f7884a9d62e329ee9a780c238a0372f9a2a3
metadream <app@zerg.cc>
2023-11-18 17:48:02 +0800

    Update Toast.js

--- src/components/Toast.js
+++ src/components/Toast.js
@@ -5,22 +5,20 @@ import { Component } from './Component.js';
 /**
  * 悬浮消息组件
  * @example <tag type="warning|error|success">text</tag>
- * @example Quick.info(text)
- * @example Quick.warning(text)
- * @example Quick.error(text)
- * @example Quick.success(text)
  */
 export class Toast extends Component {
 
     static styles = styles;
+    static attrs = ['type'];
     static template = `<div class="overlay toast"><div><slot></slot></div></div>`;
+
+    onChanged(_, value) {
+        this.query('.toast>div').addClass(value);
+    }
 
     onConnected() {
         const $toast = this.query('.toast');
         $toast.addClass('bounce-in');
-
-        const type = this.attr('type');
-        type && $toast.querySelector('div').addClass(type);
 
         const delay = parseInteger(this.attr('delay')) || 3000;
         setTimeout(() => {
