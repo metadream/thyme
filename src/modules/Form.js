5fda55df36bea8f2e8ec0492ede6d154e0d88547
metadream <mophisoft@gmail.com>
2023-11-21 11:27:34 +0800

    Update Form.js

--- src/modules/Form.js
+++ src/modules/Form.js
@@ -1,9 +1,10 @@
-import { Locale } from './Locale.js';
-
 /**
  * 表单验证和获取
  */
 export class Form {
+
+    static email = '^\w+([_\-+\.]\w+)*@\w+([\-\.]\w+)*\.([a-zA-Z]{2,})$';
+    static url = '^https?:\/\/([\w\-]+\.){1,}[a-zA-Z]{2,6}(\/[\S]*)?$';
 
     /**
      * 将带有name属性的元素数据解析为JSON对象
@@ -15,41 +16,9 @@ export class Form {
         const data = {};
 
         for (const field of fields) {
-            let value = '';
-            const { type, tagName } = field;
+            if (!field.reportValidity()) return;
+            const { type, tagName, name, value } = field;
 
-            if ((type == 'checkbox' || type == 'radio' || tagName == 'QUICK-CHECKBOX') && !field.checked) {
-                continue;
-            }
-            if (tagName === 'INPUT' || tagName === 'TEXTAREA' || tagName.startsWith('QUICK-')) {
-                if (field.value === null || field.value === undefined) {
-                    continue;
-                }
-                if (typeof field.value === 'string') {
-                    field.value = field.value.trim();
-                }
-                value = field.value;
-            }
-            else if (tagName === 'SELECT') {
-                value = field.options[field.selectedIndex].value;
-            }
-            else if (field.isContentEditable) {
-                value = field.innerHTML = field.innerHTML.trim();
-            }
-            else {
-                value = field.textContent = field.textContent.trim();
-            }
-
-            // 数据校验
-            const required = field.attr('required');
-            if (!this.#validate(value, required, field.dataset.rule)) {
-                field.focus();
-                Quick.error(field.dataset.message || Locale.get('INCORRECT_INPUT'));
-                return;
-            }
-
-            // 设置返回对象的键值
-            const name = field.attr('name');
             if (data[name]) {
                 data[name] += ',' + value;
             } else {
@@ -64,86 +33,18 @@ export class Form {
      * @param scopes 数组内每组对象的获取范围
      */
     static getJsonArray(scopes) {
-        const array = [];
         scopes = typeof scopes === 'string' ? document.querySelectorAll(scopes) : scopes;
+        const array = [];
 
         for (const scope of scopes) {
             const obj = this.getJsonObject(scope);
             if (!obj) return; // !important
 
-            if (Object.keys(obj).length !== 0) {
+            if (Object.keys(obj).length) {
                 array.push(obj);
             }
         }
         return array;
-    }
-
-    /**
-     * 数据校验
-     * @param value 数据值
-     * @param required 是否必填
-     * @param rule 数据规则
-     */
-    static #validate(value, required, rule) {
-        if (required && !value) return false; // 必填但空值，校验失败
-        if (!rule) return true; // 无规则，视为成功
-        if (!value) return true; // 空值无需校验，视为成功
-
-        // 有规则但格式不匹配，视为成功（相当于无规则）
-        const matches = rule.match(/^(?<type>[a-z0-9]+)(\((?<min>\-?\d+)(,\s*(?<max>\-?\d+))?\))?$/);
-        if (!matches) return true;
-
-        // 有规则但找不到预设的校验方法，视为成功（相当于无规则）
-        const groups = matches.groups;
-        const fn = this.#validator[groups.type];
-        if (!fn) return true;
-
-        // 由预设的校验方法判断
-        return fn(value, groups.min, groups.max);
-    }
-
-    /**
-     * 常用类型验证器
-     */
-    static #validator = {
-        // 字符串格式
-        varchar: function (value, min, max) {
-            if (!max) { max = min, min = 0; }
-            const pattern = new RegExp('^.{' + min + ',' + max + '}$');
-            return pattern.test(value);
-        },
-
-        // 整数格式：m为整数位，允许负数
-        integer: function (value, m) {
-            const pattern = new RegExp('^\\-?\\d{0,' + m + '}$');
-            return pattern.test(value);
-        },
-
-        // 数字格式：m为整数位，d为小数位，允许负数
-        decimal: function (value, m, d) {
-            const pattern = new RegExp('^\\-?\\d{0,' + m + '}(\\.\\d{0,' + d + '})?$');
-            return pattern.test(value);
-        },
-
-        // 日期格式：yyyy-MM-dd
-        date: function (value) {
-            const m = value.match(/^(\d{4})\-(\d{2})\-(\d{2})$/);
-            if (m) {
-                const d = new Date(m[1], m[2] - 1, m[3]);
-                return d.getFullYear() == m[1] && d.getMonth() == m[2] - 1 && d.getDate() == m[3];
-            }
-            return false;
-        },
-
-        // 邮件格式
-        email: function (value) {
-            return /^\w+([_\-+.]\w+)*@\w+([-.]\w+)*\.([a-zA-Z]{2,})$/.test(value);
-        },
-
-        // 网址格式
-        url: function (value) {
-            return /^$|^(https?\:\/\/)?([a-zA-Z0-9\-]+\.){1,}[a-zA-Z]{2,6}(\/[\S]*)?$/.test(value);
-        }
     }
 
 }
