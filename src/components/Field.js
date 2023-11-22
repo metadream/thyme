6412d04408de3fc04d009d2f2a1335b307c614a8
metadream <app@zerg.cc>
2023-11-22 18:39:38 +0800

    Update Field.js

--- src/components/Field.js
+++ src/components/Field.js
@@ -108,7 +108,7 @@ export class Field extends Component {
             this.shadowRoot.append(tooltip);
             setTimeout(() => {
                 tooltip.remove();
-            }, 3000);
+            }, 5000);
         }
 
         if (validated) {
